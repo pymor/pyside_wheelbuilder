474d97eaf7db77f8500838455585f8a78198bb81
Rene Milk <rene.milk@wwu.de>
2017-07-12 14:43:14 +0200

    refactor for better names and building of multiple py version wheels

--- Makefile
+++ Makefile
@@ -1,6 +1,8 @@
 .PHONY: pyside
+
 pyside:
-	docker build -t pymor/pysidewheel:3.4 pyside_docker/
-	docker run --rm -t -e LOCAL_USER_ID=$(shell id -u) -e PYVER="cp34-cp34m" -v ${PWD}:/io pymor/pysidewheel:3.4 /usr/local/bin/build-wheels.sh
+	docker build -t pymor/pyside_wheelbuilder:latest .
+	docker run --rm -t -e LOCAL_USER_ID=$(shell id -u) -v ${PWD}:/io pymor/pyside_wheelbuilder:latest build-wheels.sh
+	ls wheelhouse
 
 
--- build-wheels.sh
+++ build-wheels.sh
@@ -1,21 +1,19 @@
 #!/bin/bash
 set -e -x
 
-if [ -z ${PYVER+x} ]; then
-    PYVER="cp34-cp34m"
-fi
+WHEEL_DIR=/io/wheelhouse
 
-# Compile wheels
-PYBIN=/opt/python/${PYVER}/bin
-WHEEL_DIR=/io/pyside_wheelhouse
+PYS="cp26-cp26m cp26-cp26mu cp27-cp27m cp27-cp27mu cp33-cp33m cp34-cp34m cp35-cp35m cp36-cp36m"
+PYS="cp33-cp33m cp34-cp34m"
 
 cd /io/pyside_src
-${PYBIN}/python setup.py build
-find . -name "libshiboken*so*" | sudo xargs  cp -t /usr/local/lib/ || echo blah
-${PYBIN}/pip wheel /io/pyside_src -w ${WHEEL_DIR}/ -v
-# cd /usr/local/src/PySide-1.2.4
-# ${PYBIN}/python setup.py bdist_wheel --qmake=/usr/lib/qt4/bin/qmake --standalone
-
+for PY in ${PYS}; do
+    PYBIN=/opt/python/${PY}/bin/
+    ${PYBIN}/python setup.py build
+    find . -name "libshiboken*so*" | sudo xargs  cp -t /usr/local/lib/ || echo blah
+    ${PYBIN}/pip wheel /io/pyside_src -w ${WHEEL_DIR}/ -v
+    sudo rm /usr/local/lib/libshiboken*
+done
 
 # Bundle external shared libraries into the wheels
 for whl in ${WHEEL_DIR}/*.whl; do
